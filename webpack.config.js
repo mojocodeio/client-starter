61ed2c2d0a3ccd3b95af080497e305a5018c4271
Joey Schrader <jschrader@overstock.com>
2019-08-23 08:41:04 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+// -- plugins -- //
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+
+// -- plugin configuration -- //
+const HtmlPluginConfig = new HtmlWebPackPlugin({
+    template: './src/public/index.html',
+    filename: './index.html'
+});
+
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },{
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        HtmlPluginConfig
+    ]
+}
